b2344611e9d087792a0e201106be4b314c771740
ilyharaksher <ilyharakshw@gmail.com>
2026-06-01 23:13:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+//Console.WriteLine(Directory.GetCurrentDirectory());
+
+bool run = true;
+
+while (run)
+{
+    Console.WriteLine("Выберите действие:\n" +
+        "1. Показать задачи\n" +
+        "2. Добавить задачу\n" +
+        "3. Переименовать задачу\n" +
+        "4. Удалить задачу\n" +
+        "5. Отсортировать id задач\n" +
+        "0. Закончить работу\n");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    if (choice == 1) // Показать всё
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var tasks = db.Tasks.ToList();
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Id: {task.Id} Название: {task.Name}");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    else if (choice == 2) // Добавить задачу
+    {
+        Console.WriteLine("Введите название новой задачи");
+        string? newTaskName = Console.ReadLine();
+
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            Task newTask = new Task { Name = newTaskName };
+            db.Tasks.Add(newTask);
+            db.SaveChanges();
+        }
+        Console.WriteLine();
+    }
+
+    else if (choice == 3)  // Переименовать задачу
+    {
+        Console.WriteLine("Введите Id задачи, которую хотите переименовать");
+        int updatingId = Convert.ToInt32(Console.ReadLine());
+
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            Task? updatingTask = db.Tasks.Find(updatingId);
+            if (updatingTask != null)
+            {
+                Console.WriteLine("Введите новое название");
+                string updatingTaskNewName = Console.ReadLine();
+                updatingTask.Name = updatingTaskNewName;
+                db.Tasks.Update(updatingTask);
+                db.SaveChanges();
+                Console.WriteLine("Задача успешно обновлена");
+            }
+            else
+            {
+                Console.WriteLine("Задачи с таким Id нет");
+            }
+        }
+        Console.WriteLine();
+    }
+    else if (choice == 4) // удалить задачу
+    {
+        Console.WriteLine("Введите ID удаляемой задачи");
+        int removing_id = Convert.ToInt32(Console.ReadLine());
+
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            Task? removingTask = db.Tasks.Find(removing_id);
+
+            if (removingTask != null)
+            {
+                db.Remove(removingTask);
+                db.SaveChanges();
+                Console.WriteLine("Задача успешно удалена!");
+            }
+            else
+            {
+                Console.WriteLine($"Задачи с Id {removing_id} нет");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    else if (choice == 0)
+    {
+        Console.WriteLine("Работа завершена! Для выхода нажмите ENTER");
+        Console.ReadLine();
+        run = false;
+    }
+}
+
+
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Task> Tasks => Set<Task>();
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Tasks.db");
+    }
+}
+
+public class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; } = false;
+}
