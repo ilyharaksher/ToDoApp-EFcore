a431189f8b84664cdaa3b7733056b84e2f407f40
ilyharaksher <ilyharakshw@gmail.com>
2026-06-02 15:19:50 +0300

    adding files for debuilding process

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp_EFcore.Data
+{
+    internal class ApplicationContext
+    {
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp_EFcore.Models
+{
+    internal class Task
+    {
+    }
+}
